cd6e50f9a8edba77265185a60f316923f1dfcde1
treyfcortez <treyfcortez@yahoo.com>
2023-11-08 16:08:44 -0700

    added if statements and fixed javascript code

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Random-Password
+# Random Password
+
+## This page is meant to generate a random password for anyone looking for a secure password. The user has the option to include or not include lowercase letters, uppercase letters, special characters, and numbers in their secure password. If the user fails to accurately select criteria, they will be told through alerts.
+
+## Screenshot
+![Password Generator Screenshot](./images/password%20generator%20screenshot.png)
+
+## Link to Webpage
+
+## Credits
+I used this website for any problems with syntax that I faced. https://www.w3schools.com/
+
+To select a license for my README, I used this website. https://choosealicense.com/
+
+I followed this guide to structure my README. https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+My bootcamp TA Tom Gold helped me with the if statements code from lines 71 to 88 since I was having trouble making my code choose if the user wanted to user a specific type of character or not.
+
+## License
+MIT License
+
+Copyright (c) [2023] [Trey Cortez]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var specialCharacters = ["@", "$", "*", "!", "%", "&", "#", "?", ">", ".", "=", "+", "(", "-", ")", "<", "/", "\\", ";", "[", "]", ":", "{", "}", "|", "“", "’", "_"];
-
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ['@', '$', '*', '!', '%', '&', '#', '?', '>', '.', '=', '+', '(', '-', ')', '<', '/', '\\', ';', '[', ']', ':', '{', '}', '|', '"', "'", '_'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
 // Create a prompt function to ask for options
@@ -15,93 +12,78 @@ function passwordOptions() {
   let length = parseInt(prompt('How many characters in your password. Stay between 8 and 128.'));
   //conditional statement to check if password length is a number 
   //conditional statement to check if password length is at least 8 characters long. 
-  if(isNaN(length) || length < 8 || length > 128){
+  if (isNaN(length) || length < 8 || length > 128) {
     alert("Must be between 8 and 128 characters")
-      //put a message here letting user know that password needs to be a number using alert('put message here')
     return null;
   }
-    
-  
-    let lowerCaseConfirm = confirm("Yes I want lowercase");
-    let upperCaseConfirm = confirm("Yes I want uppercase");
-    let specialCharactersConfirm = confirm("Yes I want special characters");
-    let numbersConfirm = confirm("Yes I want numbers");
-  
-  //conditional to check if user doesn't want any types of characters
 
-  if(lowerCaseConfirm === false && upperCaseConfirm === false && specialCharactersConfirm === false && numbersConfirm === false){
+  let lowerCaseConfirm = confirm("Yes I want lowercase");
+  let upperCaseConfirm = confirm("Yes I want uppercase");
+  let specialCharactersConfirm = confirm("Yes I want special characters");
+  let numbersConfirm = confirm("Yes I want numbers");
+
+//conditional to check if user doesn't want any types of characters
+  if (lowerCaseConfirm === false && upperCaseConfirm === false && specialCharactersConfirm === false && numbersConfirm === false) {
     alert("You must choose at least one character")
-    //put in alert message telling the user at least 8 characters
+    //put in alert message telling the user at least one character
     return null;
   }
-//object store to user input for length and other characters options
-    let passwordCriteria = {
+  //object store to user input for length and other characters options
+  let passwordCriteria = {
 
-      lowerCaseConfirm: lowerCaseConfirm,
-      upperCaseConfirm: upperCaseConfirm,
-      specialCharactersConfirm: specialCharactersConfirm,
-      numbersConfirm: numbersConfirm,
-      length: length,
-    }
-    return passwordCriteria;
+    lowerCaseConfirm: lowerCaseConfirm,
+    upperCaseConfirm: upperCaseConfirm,
+    specialCharactersConfirm: specialCharactersConfirm,
+    numbersConfirm: numbersConfirm,
+    length: length,
   }
+  console.log(length)
 
-
-//function to generate random element in array
-// function generatePassword() {
-
-//   let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//   let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//   let specialCharacters = ["@", "$", "*", "!", "%", "&", "#", "?", ">", ".", "=", "+", "(", "-", ")", "<", "/", "\\", ";", "[", "]", ":", "{", "}", "|", "“", "’", "_"];
-//   let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  
-//   return generatePassword
-// }
+  return passwordCriteria;
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
-  
+
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", passwordOptions);
+generateBtn.addEventListener("click", writePassword);
 
-
+function randomNum(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length)
+  var randomEl = arr[randomIndex]
+  return randomEl
+}
 // Generate the password
 function generatePassword() {
-let password = "";
-
-const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const specialCharacters = ["@", "$", "*", "!", "%", "&", "#", "?", ">", ".", "=", "+", "(", "-", ")", "<", "/", "\\", ";", "[", "]", ":", "{", "}", "|", "“", "’", "_"];
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let result = lowerCase.concat(" ", upperCase, " ", specialCharacters, " ", numbers);
-
-// var randomCharacter = randomCharacterSet[Math.floor(Math.random() * randomCharacterSet.length)];
-  // for(var i = 0; i < passwordLength; i++) {
-function generatePassword() {
-  let password = "";
-  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-  password += numbers[Math.floor(Math.random() * numbers.length)];
+  let options = passwordOptions()
+  console.log(options)
+  let allCharacters = []
+  let passwordValue = []
   
-  while(length > password.length){
-    password += result[Math.floor(Math.random() * result.length)];
+  if (options.lowerCaseConfirm) {
+    allCharacters = allCharacters.concat(lowerCase)
   }
-  passwordBox.value = password;
-   password += lowerCase;
-   password += upperCase;
-   password += specialCharacters;
-   password += numbers;
-
+  if (options.upperCaseConfirm) {
+    allCharacters = allCharacters.concat(upperCase)
   }
-
-
-   return password;
+  if (options.specialCharactersConfirm) {
+    allCharacters = allCharacters.concat(specialCharacters)
+  }
+  if (options.numbersConfirm) {
+    allCharacters = allCharacters.concat(numbers)
+  }
+console.log(allCharacters)
+  for (var i = 0; i < options.length; i++) {
+    passwordValue.push(randomNum(allCharacters))
+  }
+  console.log(passwordValue)
+  return passwordValue.join("")
 }
